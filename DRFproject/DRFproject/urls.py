a2544de00443cf572eb327e2384908233ef2a900
IamGOOD <rukshenas1990@gmail.com>
2022-12-13 00:02:23 +0100

    ViewSets

--- DRFproject/DRFproject/urls.py
+++ DRFproject/DRFproject/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from stars.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'stars', StarsViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/starslist/', StarsAPIList.as_view()),
-    path('api/v1/starslist/<int:pk>/', StarsAPIUpdate.as_view()),
-    path('api/v1/starsdetail/<int:pk>/', StarsAPIDetailView.as_view()),
-
+    path('api/v1/', include(router.urls)), #http://127.0.0.1:8000/api/v1/stars/
+    #path('api/v1/starslist/', StarsViewSet.as_view({'get': 'list'})),
+    #path('api/v1/starslist/<int:pk>/', StarsViewSet.as_view({'put': 'update'})),
 ]
